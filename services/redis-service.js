85cdc10bed7e39fcd11381fab13c746f3260e0e8
unknown <jaynikpatel119977.jp@gmail.com>
2022-04-26 16:13:21 +0530

    console redis ip

--- services/redis-service.js
+++ services/redis-service.js
@@ -1,5 +1,6 @@
 const redis = require('redis');
 // const client = redis.createClient();
+console.log(process.env.REDIS_IP + "  " + process.env.REDIS_PORT);
 const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_IP);
 client.connect();
 client.on('connect', function () {
